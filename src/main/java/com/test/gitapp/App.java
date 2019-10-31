831c9d87857686084ab8c261363559a81a8f1d33
dell <dell@DESKTOP-H8VIF51>
2019-10-31 21:31:38 +0530

    Added add method

--- src/main/java/com/test/gitapp/App.java
+++ src/main/java/com/test/gitapp/App.java
@@ -5,5 +5,7 @@ public class App
 {
 	public static void main(String[] args) {
 		System.out.println("Github initial commit");
+		Calculate calc=new Calculate();
+		System.out.println(calc.add(10, 20));
 	}
 }
---
+++ src/main/java/com/test/gitapp/Calculate.java
@@ -0,0 +1,9 @@
+package com.test.gitapp;
+
+public class Calculate {
+
+	
+	public int add(int a,int b) {
+		return a+b;
+	}
+}
